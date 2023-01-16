d251c334fd750a7826c1216de9676bfefd210403
Eltun Ismayilov <eltunhi@code.edu.az>
2023-01-16 15:59:56 +0400

    Zooy 1/16/2023

--- ZooY/DataAccess/Repositories/Concrete/ProductRepository.cs
+++ ZooY/DataAccess/Repositories/Concrete/ProductRepository.cs
@@ -2,11 +2,7 @@
 using DataAccess.Contexts;
 using DataAccess.Repositories.Abstract;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace DataAccess.Repositories.Concrete
 {
